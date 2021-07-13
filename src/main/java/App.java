5c811aa595b309465058241aa3a9d9ea2b7cb86c
Vars <senyavasilenko@yandex.ru>
2021-07-13 16:22:53 +0300

    First commit 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld secondBean = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanFirstCat = (Cat) applicationContext.getBean("cat");
+        Cat beanSecondCat = (Cat) applicationContext.getBean("cat");
+        System.out.printf("First comparison result: %s%n", bean.equals(secondBean));
+        System.out.printf("First comparison result: %s%n", beanFirstCat.equals(beanSecondCat));
     }
 }
